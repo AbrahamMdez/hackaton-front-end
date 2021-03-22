abd0d692ea5d967f0daa2f7f73964c163243b641
mariannamaldonado <mariannamldndo@gmail.com>
2021-03-22 13:05:26 +0100

    actualizacion de vistas

---
+++ src/views/Achievements.vue
@@ -0,0 +1,27 @@
+<template>
+  <main>
+    <NavBar />
+    <NavBarFooter />
+ <h1>Logros</h1>
+ <section>
+     <article>
+          
+     </article>
+ </section>
+
+  </main>
+</template>
+
+<script>
+import NavBar from '@/components/NavBar.vue'
+import NavBarFooter from '@/components/NavBarFooter.vue'
+
+
+export default {
+  name: 'Achievements',
+  components: {
+    NavBar,
+    NavBarFooter
+  }
+}
+</script>
